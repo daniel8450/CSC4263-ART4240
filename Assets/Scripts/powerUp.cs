0aa89ff1907e3e4f40c700434c671d3526eb610f
BuildTools <unconfigured@null.spigotmc.org>
2017-03-05 23:45:30 -0600

    added project and some scripts
    added cat.cs, control.cs, and powerUp.cs

---
+++ Assets/Scripts/powerUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerUp : MonoBehaviour
+{
+    public GameObject obj;
+    public Cat cat;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Cat"))
+        {
+            if (obj.name == "catNip")
+            {
+                Debug.Log("speedUP");
+                cat.speed = 5;
+            }
+
+            if (obj.name == "milk")
+            {
+                cat.life += 1;
+                Debug.Log("lifeUP");
+            }
+
+            if (obj.name == "food")
+            {
+                Debug.Log("healthUP");
+                cat.health += 2;
+            }
+
+            Destroy(obj);
+        }
+
+    }
+
+
+
+
+
+	
+}
