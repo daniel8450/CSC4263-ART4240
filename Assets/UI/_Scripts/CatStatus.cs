2a5d7c355f18087b88c3cc6f2fe1be15c7a0a7c2
Julian Dane Plaisance <jplais8@lsu.edu>
2017-03-18 18:59:07 -0500

    Added basic UI for the GamePlay Screen
    UI is kept in a prefab to place into any scene

---
+++ Assets/UI/_Scripts/CatStatus.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatStatus : MonoBehaviour
+{
+	public Text livesText;
+	public Text PogoText;
+	public Cat playerCat;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GetCatLives();
+		GetCatPogo();
+	}
+
+	void GetCatSpeed ()
+	{
+
+	}
+
+	void GetCatLives ()
+	{
+		livesText.text = "" + playerCat.life;
+	}
+
+	void GetCatPogo ()
+	{
+		PogoText.text = "PogoStick: " + playerCat.pogo;
+	}
+}
