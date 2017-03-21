7271600d7d9bccb639010666198581cae8879a1d
vidella-lee <lizinlot@gmail.com>
2017-03-20 19:33:45 -0500

    Script to Flip Sprites
    Flips image along x-axis through scaling

---
+++ Assets/Scripts/flipImage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flipImage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }//reverses the character by scaling in negative direction along x
+    }
+}
