ce7274d917624189e6b9234ff88170f951b9176f
vidella-lee <lizinlot@gmail.com>
2017-03-20 19:30:05 -0500

    Fixed Enemy Movement

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -5,14 +5,13 @@ public class EnemyBehavior : MonoBehaviour
 {
 
     public Transform target;
-    Quaternion enemyRotation;
     Vector2 playerPos, enemyPos;
     public float moveSpeed;
 
 
     void Start()
     {
-        enemyRotation = this.transform.localRotation;
+      
     }
 
     void Update()
@@ -22,20 +21,17 @@ public class EnemyBehavior : MonoBehaviour
         if (Vector2.Distance(transform.transform.position, target.transform.position) < 10)//move towards if player gets too close
         {
             transform.position = Vector2.MoveTowards(enemyPos, playerPos, moveSpeed*Time.deltaTime);
-            //Vector2 velocity = new Vector2((transform.position.x - target.position.x) * moveSpeed, (transform.position.y - target.position.y) * 0);
-            //GetComponent<Rigidbody2D>().velocity = -velocity;
+       
         }
     
 
-        if (target.position.x > transform.position.x)//rotates enemy to the right if player is to the right  
+        if (target.position.x > transform.position.x)//flips enemy to the right if player is to the right  
         {
-            enemyRotation.x = 180;
-            transform.localRotation = enemyRotation;
+            transform.localScale = new Vector3(-0.3f, 0.3f, 1);
         }
-        if (target.position.x < transform.position.x)//rotates enemy to the left if player is to the left 
+        if (target.position.x < transform.position.x)//flips enemy to the left if player is to the left 
         {
-            enemyRotation.x = 0;
-            transform.localRotation = enemyRotation;
+            transform.localScale = new Vector3(0.3f, 0.3f, 1);
         }
     }
     }
