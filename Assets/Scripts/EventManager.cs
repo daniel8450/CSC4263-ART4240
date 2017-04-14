508892870a668f4c467ea3394c6a951d82ae885a
Christopher Gore <cagore10@gmail.com>
2017-04-13 22:45:20 -0500

    Created switch object and test environment
    Created switch object used to spawn in other objects and a scene to use
    as a test environment.  Pogo spawn in on switch not working correctly.

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour 
+{
+
+	public Cat cat;
+	public GameObject obj;
+	public GameObject pogo;
+	SpriteRenderer renderer;
+
+	void Start()
+	{
+		renderer = gameObject.GetComponent<SpriteRenderer>();
+		//obj = GetComponent<CircleCollider2D> ();
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.CompareTag("Cat"))
+		{
+			//GameObject.FindGameObjectWithTag ("switch").activeSelf = false;
+			//GameObject ref = Instantiate(myObj, myPosition, myRotation) as GameObject; 
+			//GameObject re = Instantiate(3, -3, 0) as GameObject; 
+			Spawn();
+			//obj.GetComponent<CircleCollider2D> ().isTrigger;
+			renderer.color = Color.blue;
+		}
+	}
+
+	void Spawn()
+	{
+
+		Instantiate(pogo, new Vector3(3, -3), Quaternion.identity);
+	}
+}
