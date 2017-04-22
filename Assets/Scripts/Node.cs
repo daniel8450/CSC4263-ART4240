73ff2b1078555b50e59033a4614b069b2fbc37ed
vidella-lee <lizinlot@gmail.com>
2017-04-22 09:47:25 -0500

    Pathfinding for Enemies Scripts
    Not completed.

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public List<Node> adjacent = new List<Node>;
+    public Node previous = null;
+    public string label = "";
+
+    public void Clear()
+    {
+        previous = null;
+    }
+}
