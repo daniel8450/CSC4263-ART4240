0d65d35eb0aa6d178068e6bf1239e12e0b4d91fb
Julian Dane Plaisance <jplais8@lsu.edu>
2017-04-24 12:44:09 -0500

    added MainMenu functionality

--- Assets/UI/_Scripts/MainMenu.cs
+++ Assets/UI/_Scripts/MainMenu.cs
@@ -5,25 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+	public GameObject cheatScreen;
 	// Use this for initialization
 	void Start ()
 	{
-		
-	}
-
-	public void StartButton()
-	{
-		//SceneManager.SetActiveScene(1);
+        cheatScreen.SetActive(false);
 	}
 	
 	public void CheatButton()
 	{
-
+		cheatScreen.SetActive(true);
 	}
 
 	public void QuitButton()
 	{
 		Application.Quit();
+	}
+
+	public void BackButton()
+	{
+		cheatScreen.SetActive(false);
+	}
+
+	public void GoArea(string area)
+	{
+        System.Console.WriteLine(area);
+		SceneManager.LoadScene(area);
 	}
 }
