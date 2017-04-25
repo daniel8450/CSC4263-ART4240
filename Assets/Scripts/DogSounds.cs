2e0ea6e0f5819ee9ee1b0e41b233ca0170c4086c
Julian Dane Plaisance <jplais8@lsu.edu>
2017-04-24 22:46:10 -0500

    added dog sounds
    3D dog growling/barking added

---
+++ Assets/Scripts/DogSounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogSounds : MonoBehaviour
+{
+    public AudioClip[] dogSounds;
+    public AudioSource audioSource;
+    public float time = 50f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (time <= 0)
+        {
+            audioSource.clip = dogSounds[Random.Range(0, 16)];
+            audioSource.Play();
+            time = Random.Range(125f, 475f);
+        }
+        else
+        {
+            time--;
+        }
+	}
+}
